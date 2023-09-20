f5f2270382042d9d8f82b9516314f0bd25708c0d
yamadaaiki <aiki-yamada@chiba-u.jp>
2023-09-20 17:24:07 +0900

    for warning

--- vision/transforms/transforms.py
+++ vision/transforms/transforms.py
@@ -244,7 +244,10 @@ class RandomSampleCrop(object):
         height, width, _ = image.shape
         while True:
             # randomly choose a mode
-            mode = random.choice(self.sample_options)
+            # mode = random.choice(self.sample_options)
+            random_idx = random.randint(0, len(self.sample_options) - 1)
+            mode = self.sample_options[random_idx]
+            
             if mode is None:
                 return image, boxes, labels
 
